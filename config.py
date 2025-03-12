d8eff89b18adfdd6688eca034dbd0e1022665614
= <sultaninstem004@gmail.com>
2025-03-12 13:21:31 -0700

    minor

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SCREEN_SIZE = (1200, 800)
+SCREEN_SIZE = (1100, 600)
 GRID_SIZE = (300, 600) ### the typical grid must be 1x2 ratio 
--- grid.py
+++ grid.py
@@ -2,12 +2,12 @@ from config import SCREEN_SIZE, GRID_SIZE
 import pygame
 class Grid: 
     def __init__(self): ## size is usually 10x20 ratio
-        pass
+        self.pos = ((SCREEN_SIZE[0] - GRID_SIZE[0]) // 2,0)
 
     def draw(self, screen): 
         pygame.draw.rect(
             screen, 
             (255,0,0), 
-            (100,100,GRID_SIZE[0],GRID_SIZE[1]),
-            0
+            (self.pos[0],self.pos[1],GRID_SIZE[0],GRID_SIZE[1]),
+            1
         )
