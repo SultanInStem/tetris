da8e7b4be75a79f56498a24403ac5d19195be330
= <sultaninstem004@gmail.com>
2025-05-19 11:33:35 -0700

    config

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 SCREEN_SIZE = (1100, 700)
 GRID_SIZE = (300, 600) ### the typical grid must be 1x2 ratio 
+BLOCK_SIZE = GRID_SIZE[0] // 10
--- grid.py
+++ grid.py
@@ -1,9 +1,8 @@
-from config import SCREEN_SIZE, GRID_SIZE
+from config import SCREEN_SIZE, GRID_SIZE, BLOCK_SIZE
 import pygame
 class Grid: 
     def __init__(self): ## size is usually 10x20 ratio
         self.pos = ((SCREEN_SIZE[0] - GRID_SIZE[0]) // 2, 0)
-        self.block_size = GRID_SIZE[0] // 10
 
     def draw(self, screen): 
         pygame.draw.rect(
@@ -13,19 +12,19 @@ class Grid:
             1
         )
 
-        for x in range(0, GRID_SIZE[0] // self.block_size): 
+        for x in range(0, GRID_SIZE[0] // BLOCK_SIZE): 
             pygame.draw.line(
                 screen, 
                 (255,0,0), 
-                (self.pos[0] + self.block_size * x, 0), 
-                (self.pos[0] + self.block_size * x, GRID_SIZE[1]), 
+                (self.pos[0] + BLOCK_SIZE * x, 0), 
+                (self.pos[0] + BLOCK_SIZE * x, GRID_SIZE[1]), 
                 1
             )
-        for y in range (0, GRID_SIZE[1] // self.block_size): 
+        for y in range (0, GRID_SIZE[1] // BLOCK_SIZE): 
             pygame.draw.line(
                 screen, 
                 (0,255,0), 
-                (self.pos[0], self.pos[1] + self.block_size * y),
-                (self.pos[0] + GRID_SIZE[0], self.pos[1] + self.block_size * y),
+                (self.pos[0], self.pos[1] + BLOCK_SIZE * y),
+                (self.pos[0] + GRID_SIZE[0], self.pos[1] + BLOCK_SIZE * y),
                 1
             )
